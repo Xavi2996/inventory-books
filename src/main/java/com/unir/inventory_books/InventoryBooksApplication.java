c74bdafc1e323b1f182eac4ea3211531b80acc38
xavier <xavier.cobos.29@hotmail.com>
2025-02-02 20:44:40 -0500

    configuracion del proyeecto

---
+++ src/main/java/com/unir/inventory_books/InventoryBooksApplication.java
@@ -0,0 +1,13 @@
+package com.unir.inventory_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/inventory_books/InventoryBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.inventory_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
